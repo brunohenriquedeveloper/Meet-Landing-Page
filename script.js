c6a433faa17f985284312440760377f0db33f22b
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-11-07 12:48:43 -0300

    End the project

--- script.js
+++ script.js
@@ -7,29 +7,34 @@ document.addEventListener("DOMContentLoaded", function() {
     const errorEmail = document.getElementById("error-email");
     const dimiss = document.getElementById("dimiss")
     const submitted = document.querySelector(".submitted-email")
+    const style = document.createElement('style')
+    document.head.appendChild(style)
 
     function togglelayouts(){
         subscription.classList.toggle("hidden")
         success.classList.toggle("hidden")
+        
     }
 
     form.addEventListener("submit", function(event){
+        event.preventDefault() // evita envio do form
         if(!emailValid(emailField.value)){
-            event.preventDefault() // evita envio do form
             errorEmail.style.display = "inline"
             emailField.style.backgroundColor = "#FFE8E6"
             emailField.style.color = "hsl(4, 100%, 67%)"
             emailField.style.borderColor = "hsl(4, 100%, 67%)"
-            const style = document.createElement('style')
              style.innerHTML = `#inemail::placeholder { color: hsl(4, 100%, 67%); }`; 
              document.head.appendChild(style)
             label.style.marginBottom = "-2em"
         } else {
             errorEmail.style.display = "none"
-            event.preventDefault()
             togglelayouts()
             submitted.innerText = emailField.value
             emailField.value = ""
+            emailField.style.backgroundColor = "white"
+            emailField.style.borderColor = "black"
+            style.innerHTML = `#inemail::placeholder { color: hsl(231, 7%, 60%); }`
+           
         }
     })
 
@@ -41,7 +46,15 @@ document.addEventListener("DOMContentLoaded", function() {
         );
     }
 
-    dimiss.addEventListener("click", togglelayouts)
+    dimiss.addEventListener("click", function(){
+        togglelayouts()
+           // Redefine o estilo do campo de e-mail ao estado inicial
+           emailField.style.backgroundColor = "white";
+           emailField.style.borderColor = "black";
+           emailField.style.color = "initial";
+           style.innerHTML = `#inemail::placeholder { color: hsl(231, 7%, 60%); }`;
+           label.style.marginBottom = ""
+    })
     
     
 })
